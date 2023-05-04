bfcd0bd2d989b75662579fa478b58c8831493447
Faleh Alotaibi <121350271+f-alotaibi@users.noreply.github.com>
2023-05-04 10:30:56 +0300

    Better env naming

--- DBCon.js
+++ DBCon.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql2/promise');
 // connect to the db
 dbConnectionInfo = {
-  host: process.env.MY_HOST,
-  port: process.env.MY_PORT,
-  user: process.env.MY_USER,
-  password: process.env.MY_PASSWORD,
-  database: process.env.MY_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASENAME,
 };
 
 //create mysql connection pool
